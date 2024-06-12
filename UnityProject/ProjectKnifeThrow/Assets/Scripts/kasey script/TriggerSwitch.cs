637f448bb69705542859f7997a0100d37d1da402
dioavila <49199648+dioavila@users.noreply.github.com>
2024-06-12 00:17:38 -0400

    Edit Script Switch

--- UnityProject/ProjectKnifeThrow/Assets/Scripts/kasey script/TriggerSwitch.cs
+++ UnityProject/ProjectKnifeThrow/Assets/Scripts/kasey script/TriggerSwitch.cs
@@ -1,24 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 //This trigger only works with the knife throw && has to be the standard knife
 
 public class TriggerSwitch : MonoBehaviour
 {
+    [Header("Movement Setup")]
     [SerializeField] GameObject obj;
+    [SerializeField] Transform Position;
+    [SerializeField] float extendRate = 1f;
 
-    [SerializeField] Vector3 Position;
-
+    [Header("Timer Setup")]
+    [SerializeField] float timerToRetract;
+    [SerializeField] float coolDownTimer;
+    [SerializeField] Transform startPos;
     Renderer Model;
-
+    
     public bool Active;
-
 
     // Start is called before the first frame update
     void Start()
     {
         Model = gameObject.GetComponent<Renderer>();
+       // startPos = obj.transform;
     }
 
     // Update is called once per frame
@@ -27,10 +33,14 @@ public class TriggerSwitch : MonoBehaviour
         if (Active)
         {
             Model.material.color = Color.green;
+            MoveObj(Active);
+
         }
         if (!Active) 
         {
             Model.material.color = Color.red;
+            MoveObj(Active);
+
         }
     }
 
@@ -38,13 +48,23 @@ public class TriggerSwitch : MonoBehaviour
     {
         if (collision.gameObject.name == "Standard Knife(Clone)" && !Active)
         {
-            obj.transform.position += new Vector3(Position.x, Position.y, Position.z);
             Active = true;
         }
         else
         {
-            obj.transform.position -= new Vector3(Position.x, Position.y, Position.z);
             Active = false;
+        }
+    }
+
+    void MoveObj(bool activeStatus)
+    {
+        if(activeStatus)
+        {
+            obj.transform.position = Vector3.Lerp(obj.transform.position, new Vector3(startPos.position.x, Position.position.y, startPos.position.z), Time.deltaTime * extendRate);
+        }
+        else
+        {
+            obj.transform.position = Vector3.Lerp(obj.transform.position, startPos.position, Time.deltaTime * extendRate);
         }
     }
 }
